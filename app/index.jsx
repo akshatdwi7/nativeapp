7405558c632cd39cc9fd92d527cdeb0b63d1473a
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-02-24 00:05:03 +0530

    g
    b

--- app/index.jsx
+++ app/index.jsx
@@ -1,18 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
 import { Link } from "expo-router";
+import { View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Life is sweet!</Text>
+    <View className="flex-1 items-center justify-center bg-orange-200">
+      <Text className="text-3xl">Its a ne day!</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" style={{ color: "blue" }}>
-        Tap to go to Profile
-      </Link>
-      <Link href="/settings" style={{ color: "purple" }}>
-        Settings and make it aesthetic
-      </Link>
     </View>
   );
 }
